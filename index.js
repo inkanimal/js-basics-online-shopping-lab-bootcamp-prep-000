eba6382fc688ecd211bb2f6fa0d857f9e4aa8449
geoffrey woodcock <theatomproject@gmail.com>
2018-01-25 00:34:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,16 @@ function addToCart(item) {
 }
 
 function viewCart() {
+  function removeFromCart(item) {
   // write your code here
+  for (var i=0; i<cart.length; i++) {
+    if (Object.keys(cart[i]) == item) {
+      cart = [...cart.slice(0,i), ...cart.slice(i+1)];
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.");
+  return cart;// write your code here
 }
 
 function total() {
